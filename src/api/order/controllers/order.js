672e3eac945a0ed2ee08c82393bcd90a433771ae
felipe1120gomez <felipe92agudelo@gmail.com>
2022-09-17 21:04:19 -0500

    fix typo in order

--- src/api/order/controllers/order.js
+++ src/api/order/controllers/order.js
@@ -5,7 +5,7 @@
  */
 
 const { createCoreController } = require("@strapi/strapi").factories;
-const stripe = require("stripe")(process.env(STRIPE_TOKEN));
+const stripe = require("stripe")(process.env.STRIPE_TOKEN);
 
 module.exports = createCoreController("api::order.order", ({ strapi }) => ({
   async create(ctx) {
